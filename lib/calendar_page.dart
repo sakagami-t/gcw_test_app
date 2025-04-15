223e712500b1c041a26113738f1019427cbdf0e0
sakagami-t <sakagamit@nihonsoft.co.jp>
2025-04-15 16:51:44 +0900

    Add CalendarPage with a calendar widget and dummy events
    * **lib/calendar_page.dart**
      - Import `package:flutter/material.dart` and `package:table_calendar/table_calendar.dart`
      - Create `CalendarPage` class extending `StatelessWidget`
      - Add a `Calendar` widget with dummy events
    
    * **lib/main.dart**
      - Import `lib/calendar_page.dart`
      - Update `MyHomePage` to navigate to `CalendarPage`
      - Add a button to navigate to `CalendarPage`

---
+++ lib/calendar_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class CalendarPage extends StatelessWidget {
+  const CalendarPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calendar Page'),
+      ),
+      body: TableCalendar(
+        firstDay: DateTime.utc(2020, 1, 1),
+        lastDay: DateTime.utc(2030, 12, 31),
+        focusedDay: DateTime.now(),
+        eventLoader: (day) {
+          if (day.day == DateTime.now().day) {
+            return ['Dummy Event'];
+          }
+          return [];
+        },
+      ),
+    );
+  }
+}
