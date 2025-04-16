8156004a91c2e32de94ffa8652db0ab703ccaf3f
sakagami-t <sakagamit@nihonsoft.co.jp>
2025-04-16 12:01:42 +0900

    依存関係を更新し、intlパッケージを追加。table_calendarのバージョンを3.1.3に変更。flutter_localizationsを明示的に追加。

--- lib/calendar_page.dart
+++ lib/calendar_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
-import 'package:intl/intl.dart';
 
 class CalendarPage extends StatelessWidget {
   const CalendarPage({super.key});
@@ -15,7 +14,7 @@ class CalendarPage extends StatelessWidget {
         firstDay: DateTime.utc(2020, 1, 1),
         lastDay: DateTime.utc(2030, 12, 31),
         focusedDay: DateTime.now(),
-        locale: const Locale('ja'),
+        locale: 'ja',
         eventLoader: (day) {
           if (day.day == DateTime.now().day) {
             return ['Dummy Event'];
